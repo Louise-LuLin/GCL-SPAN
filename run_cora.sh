c25cfe4bfa0b336603583dc6553867be7ea1d60d
Lu Lin <lxl5598@i4-l-jzc5917-01.ad.psu.edu>
2023-06-22 14:24:07 -0700

    rm debug file

--- run_cora.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-
-DATA=$1
-DEV=$2
-
-for LR in 10 50 100 200
-do
-    python3 unsupervised_node.py --device=$DEV --dataset=$DATA --aug_lr1=$LR --aug_lr2=0.001 --aug_iter=20 --pe=0.2 &> "./results/"$DATA\_$LR\_"0.1_0.2.txt"
-done
